fc7ace201436d4888322b9e558573126cc75be87
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-12-20 11:03:47 +0100

    Tile: animated curtain on click

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,44 @@
 sixtyfps::sixtyfps! {
     MemoryTile := Rectangle {
+        signal clicked;
+        property <bool> open_curtain;
+        property <bool> solved;
+        property <resource> icon;
+
         width: 64px;
         height: 64px;
-        color: #3960D5;
+        color: solved? #34CE57 : #3960D5;
+        animate color { duration: 800ms; }
 
         Image {
             source: img!"icons/bus.png";
             width: parent.width;
             height: parent.height;
         }
+        left_curtain := Rectangle {
+            color: #193076;
+            width: open_curtain ? 0px : (parent.width / 2);
+            height: parent.height;
+            animate width { duration: 250ms; easing: ease-in; }
+        }
+        right_curtain := Rectangle {
+            color: #193076;
+            x: open_curtain ? parent.width : (parent.width / 2);
+            width: open_curtain ? 0px : (parent.width / 2);
+            height: parent.height;
+            animate width { duration: 250ms; easing: ease-in; }
+            animate x { duration: 250ms; easing: ease-in; }
+        }
+
+        TouchArea {
+            clicked => { root.clicked() }
+        }
     }
     MainWindow := Window {
-        MemoryTile {}
+        MemoryTile {
+            icon: img!"icons/bus.png";
+            clicked => { self.open_curtain = !self.open_curtain; }
+        }
     }
 }
 
