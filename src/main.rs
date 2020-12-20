cd49abceb8b7b4eae3b4018ceea604684502165e
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-12-20 18:16:19 +0100

    Complete memory game!

--- src/main.rs
+++ src/main.rs
@@ -14,6 +14,7 @@ sixtyfps::sixtyfps! {
 
         width: 64px;
         height: 64px;
+        border-radius: 8px;
         color: solved? #34CE57 : #3960D5;
         animate color { duration: 800ms; }
 
@@ -27,6 +28,7 @@ sixtyfps::sixtyfps! {
             width: open_curtain ? 0px : (parent.width / 2);
             height: parent.height;
             animate width { duration: 250ms; easing: ease-in; }
+
         }
         right_curtain := Rectangle {
             color: #193076;
@@ -42,8 +44,12 @@ sixtyfps::sixtyfps! {
         }
     }
     MainWindow := Window {
+        signal check_if_pair_solved();
+        property <bool> disable_tiles;
+        // dimensions
         width: 326px;
         height: 326px;
+        // to create the model
         property <[TileData]> memory_tiles: [
             { image: img!"icons/at.png" },
             { image: img!"icons/balance-scale.png" },
@@ -64,7 +70,10 @@ sixtyfps::sixtyfps! {
             // propagate the solved status from the model to the tile
             solved: tile.solved;
             clicked => {
-                tile.image_visible = !tile.image_visible;
+                if (!root.disable_tiles) {
+                    tile.image_visible = !tile.image_visible;
+                    root.check_if_pair_solved();
+                }
             }
         }
     }
@@ -87,6 +96,43 @@ fn main() {
     // assign shuffled vec to model
     let tiles_model = std::rc::Rc::new(sixtyfps::VecModel::from(tiles));
     main_window.set_memory_tiles(sixtyfps::ModelHandle::new(tiles_model.clone()));
+
+    // handlers
+    let close_delay = std::time::Duration::from_millis(500);
+    let mw_weak = main_window.as_weak();
+    main_window.on_check_if_pair_solved(move || {
+        let mut flipped_tiles = tiles_model
+            .iter()
+            .enumerate()
+            .filter(|(_, tile)| tile.image_visible && !tile.solved);
+
+        let pair = (flipped_tiles.next(), flipped_tiles.next());
+        match pair {
+            (Some((id1, mut tile1)), Some((id2, mut tile2))) => {
+                let is_pair_solved = tile1 == tile2;
+                if is_pair_solved {
+                    tile1.solved = true;
+                    tiles_model.set_row_data(id1, tile1.clone());
+                    tile2.solved = true;
+                    tiles_model.set_row_data(id2, tile2.clone());
+                } else {
+                    let main_window = mw_weak.unwrap();
+                    main_window.set_disable_tiles(true);
+                    let tiles_model = tiles_model.clone();
+                    sixtyfps::Timer::single_shot(close_delay, move || {
+                        main_window.set_disable_tiles(false);
+                        tile1.image_visible = false;
+                        tiles_model.set_row_data(id1, tile1);
+                        tile2.image_visible = false;
+                        tiles_model.set_row_data(id2, tile2);
+                    });
+                }
+            }
+            _ => {
+                // there's no pair of tiles
+            }
+        }
+    });
 
     // run app
     main_window.run();
