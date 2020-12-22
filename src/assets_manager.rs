840e4a2e727dc360ddb8edce64a0063dce7f9997
Rasmus Vermeulen <rasmus_vermeulen@tutanota.com>
2020-12-22 22:06:43 +0100

    Some inputs

--- src/assets_manager.rs
+++ src/assets_manager.rs
@@ -1,12 +1,42 @@
+extern crate globwalk;
+
 sixtyfps::sixtyfps! {
+
+  LabeledInput := GridLayout {
+    property <string> label <=> Label.text;
+    initial_focus: input;
+    Row {
+        Label := Text {}
+        input := TextInput {}
+    }
+}
   MainWindow := Window {
+    property <string> title: "Assets Manager App";
     width: 500px;
     height: 500px;
-    Text {
-      text: "Assets Manager App";
+    initial_focus: second_input_field;
+
+    GridLayout {
+      maximum_width: 250px;
+      maximum_height: 80px;
+      padding: 16px;
+        Row {
+            first_input_field := LabeledInput {
+              label: "first";
+            }
+        }
+        Row {
+            second_input_field := LabeledInput {
+              label: "second";
+            }
+        }
     }
   }
 }
+
 pub fn main() {
-  MainWindow::new().run();
+  use sixtyfps::SharedString;
+  let app = MainWindow::new();
+  app.set_title(SharedString::from("Assets Manager App"));
+  app.run();
 }
